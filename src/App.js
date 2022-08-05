8036c274cfd8e458cff101e302ab4ca4f104900e
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-08-05 22:43:54 +0530

    footer Added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,49 @@
 // import Land from "./components/Land";
 // import Navbar from "./components/Navbar";
 
-
-
-
-
-
 // function App() {
 //   return (
 //     <>
 //     <Navbar />
 //     <Land  />
-   
-   
-   
 
 //     </>
 //   )
 // }
 
-
 // export default App
 
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
-
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
- 
-import Land from './components/Land';
-import Shops from './components/Shops';
-import Products from './components/Products';
-import AboutUs from './components/AboutUs';
-import FAQ from './components/FAQ';
+import Land from "./components/Land";
+import Shops from "./components/Shops";
+import Products from "./components/Products";
+import AboutUs from "./components/AboutUs";
+import FAQ from "./components/FAQ";
 // import Navigation from './components/Navigation';
 import Navbar from "./components/Navbar";
- 
+import Footer from "./components/Footer";
+
 class App extends Component {
   render() {
-    return (      
-       <BrowserRouter>
+    return (
+      <BrowserRouter>
         <div>
-          <Navbar /><br />
-            <Routes>
-             <Route path="/" element={<Land />} exact/>
-             <Route path="/shops" element={<Shops />}/>
-             <Route path="/products" element={<Products />}/>
-             <Route path="/aboutus" element={<AboutUs />}/>
-             <Route path="/faq" element={<FAQ />}/>
-           </Routes>
-        </div> 
+          <Navbar />
+          <br />
+          <Routes>
+            <Route path="/" element={<Land />} exact />
+            <Route path="/shops" element={<Shops />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     );
   }
 }
- 
+
 export default App;
