3849587a7bc6ece2800b5101e85bd95a6942f940
Mushrifa <mushimmf7877@gmail.com>
2022-08-08 23:21:15 +0530

    made changes to products sign up jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -69,12 +69,8 @@ const Products = () => {
                                 </div>
 
                                 <div id="otp" class="flex flex-row justify-center text-center px-2 mt-5">
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="first" maxlength="1" />
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="second" maxlength="1" />
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="third" maxlength="1" />
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="fourth" maxlength="1" />
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="fifth" maxlength="1" />
-                                    <input class="m-2 border h-10 w-10 text-center form-control rounded" type="text" id="sixth" maxlength="1" />
+                                    <input class="m-2 border h-10 w-25 text-center form-control rounded" type="text" id="first" maxlength="6" />
+                       
                                 </div>
 
                                 <div class="flex justify-center text-center mt-5">
