fcb6d5b706be5378984677551384a5e5562d92f7
Mushrifa <mushimmf7877@gmail.com>
2022-08-11 15:07:38 +0530

    made changes in the pages of FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -23,16 +23,17 @@ export default function FAQ() {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#90E0EF] px-4 py-2 text-left text-sm font-medium text-black hover:bg-[#00B4D8] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
+                <span>WHAT ARE YOUR BUSINESS GOALS??</span>
                 <ChevronUpIcon
                   className={`${
                     open ? 'rotate-180 transform' : ''
                   } h-5 w-5 text-purple-500`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-black">
+                Our main goal is to enhance the sales of Small and medium enterprises. And provide
+                customers a great exposure to the offers, rewards and the discounts provided by the 
+                SME's.
               </Disclosure.Panel>
             </>
           )}
