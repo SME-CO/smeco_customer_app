1415ffda11759970b5a42269e73272ee7bd48f22
Mushrifa <mushimmf7877@gmail.com>
2022-09-09 20:42:27 +0530

    completed side nav bar of rcustmer

--- src/components/SideNavr.jsx
+++ src/components/SideNavr.jsx
@@ -130,6 +130,8 @@ import RecordIcon from "@mui/icons-material/History";
 import AccountIcon from "@mui/icons-material/AccountCircle";
 import SignoutIcon from "@mui/icons-material/Logout";
 import SettingsIcon from "@mui/icons-material/Settings";
+import QueriesIcon from "@mui/icons-material/MarkUnreadChatAlt";
+import logoImg from '../assets/logo.png'
 
 
 function SideNavr() {
@@ -138,9 +140,9 @@ function SideNavr() {
     <div className="hidden md:block fixed min-h-screen left-0 top-14 flex-col">
       <aside class="w-35" aria-label="Sidebar">
         <div class="overflow-y-auto py-1 px-1 bg-[#03045E] rounded dark:bg-gray-800 hover:scale-100">
-
           <ul class="space-y-2 pt-7">
             <li>
+            <img className='w-20 mx-auto mt-[-3rem] pt-3' src={logoImg} alt="" />
               <a href="#" class="flex items-center p-4 pb-5 text-base font-normal text-[#FF8C32] rounded-lg h-12 dark:text-white hover:bg-gray-100 hover:opacity-75 ">
                 <DashboardIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
                 <p class="ml-0.5 font-bold pl-3 text-[#FF8C32]">Dashboard</p>
@@ -161,14 +163,14 @@ function SideNavr() {
                 <RecordIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
                 <p class="ml-0.5 font-bold pl-3 text-[#FF8C32]">Records</p>
               </a>
-              <p class="ml-0.5 font-bold pt-14 pb-3 pl-1 text-[#FF8C32]">Settings</p>
+              <p class="ml-0.5 font-bold pt-9 pb-3 pl-1 text-[#FF8C32]">Settings</p>
               <a href="#" class="flex items-center p-4 pb-5  text-base font-normal text-[#FF8C32] rounded-lg h-12  dark:text-white hover:bg-gray-100 hover:opacity-75 ">
                 <AccountIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
                 <p class="ml-0.5 font-bold pl-3 text-[#FF8C32]">My Account</p>
               </a>
               <a href="#" class="flex items-center p-4 pb-5  text-base font-normal text-[#FF8C32] rounded-lg h-12  dark:text-white hover:bg-gray-100 hover:opacity-75 ">
-                <SettingsIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
-                <p class="ml-0.5 font-bold pl-3 text-[#FF8C32]">Settings</p>
+                <QueriesIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
+                <p class="ml-0.5 font-bold pl-3 text-[#FF8C32]">Queries</p>
               </a>
               <a href="#" class="flex items-center p-4 pb-5  text-base font-normal text-[#FF8C32] rounded-lg h-12  dark:text-white hover:bg-gray-100 hover:opacity-75 ">
                 <SignoutIcon className="!w-12 !h-12 text-[#FF8C32] p-3" />
