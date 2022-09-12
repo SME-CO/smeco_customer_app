f6bff711d688fa34098f998a3308f65394179601
Mushrifa <mushimmf7877@gmail.com>
2022-09-12 08:37:52 +0530

    completed FE of dashboard

--- src/pages/registered_customer/Dashboard.jsx
+++ src/pages/registered_customer/Dashboard.jsx
@@ -75,8 +75,11 @@
 
 import React from 'react';
 import { Doughnut } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import Navbarr from '../../components/Navbarr';
 import SideNavr from "../../components/SideNavr";
+import NotificationIcon from "@mui/icons-material/Notifications";
 import {
    Chart,
    ArcElement,
@@ -132,11 +135,14 @@ Chart.register(
    SubTitle
 );
 
+
+//doughnut chart
 const data = {
-   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+   labels: ["Appliances", "Toys & Games", "Electronics", "Sports & outdoors","Home & kitchen", "Cell Phone & Accessories"],
    datasets: [
       {
-         data: [12, 19, 3, 5, 2, 3],
+         label:'Product Categories',
+         data: [20, 35, 50, 25, 20, 75],
          backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -159,15 +165,110 @@ const data = {
 };
 
 
+//bar chart
+const data2 = {
+   labels: ["Appliances", "Toys & Games", "Electronics", "Sports & outdoors","Home & kitchen", "Cell Phone & Accessories"],
+   datasets: [
+      {
+         label:'Product Categories Count ',
+         data: [80, 85, 50, 25, 20, 75],
+         backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+         ],
+         borderColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+         ],
+         borderWidth: 1
+      },
+   ],
+};
+
+//line
+const data3 = {
+   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+   datasets: [
+      {
+         label:'Purchases ',
+         data: [20, 10, 80, 50, 60, 15],
+         backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+         ],
+         borderColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+         ],
+         borderWidth: 1
+      },
+   ],
+};
+
+
+
+
 const Dashboard = () => {
    return (
       <div>
          <Navbarr />
-         <div className='pt-24 md:ml-40 w-screen'>
-            <h1>Chart</h1>
-            <div style={{ width: "500px", margin: "0 auto" }}>
-               <Doughnut data={data} />
+         <div className='flex flex-wrap pt-24 md:ml-80 '>
+
+            {/* -------------------------------Bar Chart---------------------- */}
+            <div>
+               <div style={{ width: "500px", margin: "0 auto" }}>
+                  <Bar data={data2} />
+               </div>
             </div>
+
+            <div className='pl-32'>
+               
+               <div>
+               <p className=' shadow-2xl text-3xl p-10 my-4 rounded-lg  bg-zinc-200 '>Star Points : 100</p>
+               </div>
+               <div className='pt-4'>
+               <p className='shadow-2xl text-3xl p-10 my-4 rounded-lg  bg-zinc-200 '>Rewards : 02</p>
+               </div>
+            </div>
+
+              {/* --------------------Doughnut chart----------------------- */}
+              <div className='pr-24'>
+               <div style={{ width: "400px", margin: "0 auto" }}>
+                  <Doughnut data={data} />
+               </div>
+            </div>
+
+
+            {/* --------------------------Line------------------------- */}
+            <div className='pt-24'>
+               <div style={{ width: "500px", margin: "0 auto" }}>
+                  <Line data={data3} />
+               </div>
+            </div>
+            <br />
+
+            
+           
+
+          
+           
+
          </div>
          <SideNavr />
       </div>
